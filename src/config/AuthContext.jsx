6df10bd488bbac5069ac0236fc3fa2ad7a108592
vncntkyl <kyle.rinoza2009@gmail.com>
2024-01-02 16:04:03 +0800

    feat: add auth gateway to backend server

---
+++ src/config/AuthContext.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/prop-types */
+import axios from "axios";
+import React from "react";
+import { useContext } from "react";
+import { devEndpoints as url } from "./endpoints";
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const signInUser = async (username, password) => {
+    try {
+      const userData = new FormData();
+
+      userData.append("action", "login");
+       userData.append("username", username);
+      userData.append("password", password);
+      const response = await axios.post(url.auth, userData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const value = {
+    signInUser,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
