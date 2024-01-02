e0d66b56646940cef04328d2f2a8fa5e8398816e
vncntkyl <kyle.rinoza2009@gmail.com>
2024-01-02 16:06:42 +0800

    style: add new conditioned styles

--- src/fragments/Header.jsx
+++ src/fragments/Header.jsx
@@ -1,10 +1,11 @@
 import classNames from "classnames";
 import { Navbar } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
+import PropTypes from "prop-types";
 
 function Header() {
-    const location = useLocation();
-
+  const location = useLocation();
+  const user = localStorage.getItem("user");
   return (
     <header className="bg-white shadow-xl ">
       <Navbar>
@@ -21,7 +22,7 @@ function Header() {
                 to={`/${item}`}
                 key={index}
                 className={classNames(
-                  "capitalize font-semibold text-lg",
+                  "capitalize font-semibold text-lg hover:text-secondary",
                   location.pathname == `/${item}`
                     ? "text-secondary"
                     : "text-main"
@@ -31,10 +32,40 @@ function Header() {
               </Link>
             );
           })}
+          {user ? (
+            <button
+              className="capitalize font-semibold text-lg text-main hover:text-secondary"
+              onClick={() => localStorage.clear()}
+            >
+              Logout
+            </button>
+          ) : (
+            <>
+              {location.pathname !== "/login" ? (
+                <Link
+                  to="/login"
+                  className="capitalize font-semibold text-lg text-main hover:text-secondary"
+                >
+                  Login
+                </Link>
+              ) : (
+                <Link
+                  to="/register"
+                  className="capitalize font-semibold text-lg text-main hover:text-secondary"
+                >
+                  Register
+                </Link>
+              )}
+            </>
+          )}
         </div>
       </Navbar>
     </header>
   );
 }
 
+Header.propTypes = {
+  user: PropTypes.object,
+  setUser: PropTypes.func,
+};
 export default Header;
--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,15 +1,12 @@
-import React from "react";
-
 function Map() {
   return (
     <>
       <iframe
-        src="https://widgets.scribblemaps.com/sm/?d=true&z=true&l=true&id=rBUEqPtLeR&s"
-        allow="geolocation"
-        allowfullscreen
-        width="1080"
         height="550"
-        title="PH Map"
+        src="https://widgets.scribblemaps.com/sm/?d&dv&cv&mt&z&ol&l&gc&af&mc&dfe&lat=14.672326&lng=121.0799394&vz=15&type=custom_style&s&t=d&&height=550&id=rBUEqPtLeR"
+        style={{ border: 0, maxWidth: "100%" }}
+        allowFullScreen
+        allow="geolocation"
         loading="lazy"
       ></iframe>
     </>
