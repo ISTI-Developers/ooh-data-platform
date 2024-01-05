686a62f6abb73a2230e02ef5c6b15f8e1e6aa035
vncntkyl <kyle.rinoza2009@gmail.com>
2024-01-05 16:30:17 +0800

    feat: added responsiveness

--- src/fragments/Header.jsx
+++ src/fragments/Header.jsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import { Navbar } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
+import logo from "../assets/unai.png";
 import PropTypes from "prop-types";
 import useUser from "../config/userStore";
 
@@ -10,19 +11,35 @@ function Header() {
   const logoutUser = useUser((state) => state.clearUser);
 
   return (
-    <header className="bg-white shadow-xl ">
-      <Navbar>
+    <>
+      <Navbar
+        theme={{
+          root: {
+            base: "bg-white p-4 pb-2 dark:border-gray-700 dark:bg-gray-800 sm:px-4 shadow-md",
+          },
+          collapse: {
+            base: "w-full md:block md:w-auto animate-fade",
+          },
+
+          toggle: {
+            base: "inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-transparent hover:outline-none focus:outline-none focus:ring-0 focus:ring-none md:hidden",
+          },
+        }}
+        border
+      >
         <Navbar.Brand>
           <img
-            src="https://www.unitedneon.com/web/wp-content/uploads/2018/01/logo.png"
+            src={logo}
             alt="United Neon Advertising Inc."
+            className="w-full max-w-[200px]"
           />
         </Navbar.Brand>
-        <div className="flex items-center gap-10">
-          {["", "map", "audience", "reports"].map((item, index) => {
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          {["", "map", "audience"].map((item, index) => {
             return (
-              <Link
-                to={`/${item}`}
+              <Navbar.Link
+                href={`/${item}`}
                 key={index}
                 className={classNames(
                   "capitalize font-semibold text-lg hover:text-secondary",
@@ -32,12 +49,12 @@ function Header() {
                 )}
               >
                 {item === "" ? "planning" : item}
-              </Link>
+              </Navbar.Link>
             );
           })}
           {user ? (
             <button
-              className="capitalize font-semibold text-lg text-main hover:text-secondary"
+              className="capitalize font-semibold text-lg text-main hover:text-secondary pl-3 pt-2 md:p-0"
               onClick={() => logoutUser()}
             >
               Logout
@@ -47,23 +64,23 @@ function Header() {
               {location.pathname !== "/login" ? (
                 <Link
                   to="/login"
-                  className="capitalize font-semibold text-lg text-main hover:text-secondary"
+                  className="capitalize font-semibold text-lg text-main hover:text-secondary pl-3 pt-2 md:p-0"
                 >
                   Login
                 </Link>
               ) : (
                 <Link
                   to="/register"
-                  className="capitalize font-semibold text-lg text-main hover:text-secondary"
+                  className="capitalize font-semibold text-lg text-main hover:text-secondary pl-3 pt-2 md:p-0"
                 >
                   Register
                 </Link>
               )}
             </>
           )}
-        </div>
+        </Navbar.Collapse>
       </Navbar>
-    </header>
+    </>
   );
 }
 
