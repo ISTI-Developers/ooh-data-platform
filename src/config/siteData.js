f95d692bfe575b5c9a05be96354c096d1c1040b6
vncntkyl <kyle.rinoza2009@gmail.com>
2024-01-05 16:27:59 +0800

    refactor: updated ooh to pasyaling

--- src/config/siteData.js
+++ src/config/siteData.js
@@ -20,7 +20,7 @@ export const siteData = [
         ["social media usage"]: "Heavy Social Media Users",
         ["podcast listenership"]: "Regular Podcast Listeners",
       },
-      [`ooh behavior`]: {
+      [`pasyal-ing behavior`]: {
         [`shopping behavior`]: "Impulse Buyers",
         [`dining habits`]: "Frequent Diners",
         [`entertainment preference`]: "Event Attendees",
@@ -53,7 +53,7 @@ export const siteData = [
         ["social media usage"]: "Occasional Social Media Users",
         ["podcast listenership"]: "Occasional Podcast Listeners",
       },
-      [`ooh behavior`]: {
+      [`pasyal-ing behavior`]: {
         [`shopping behavior`]: "Intentional Shoppers",
         [`dining habits`]: "Selectively Dine Out",
         [`entertainment preference`]: "Arts and Culture Enthusiasts",
@@ -86,7 +86,7 @@ export const siteData = [
         ["social media usage"]: "Non-Social Media Users",
         ["podcast listenership"]: "Casual Podcast Listeners",
       },
-      [`ooh behavior`]: {
+      [`pasyal-ing behavior`]: {
         [`shopping behavior`]: "Exploratory Shoppers",
         [`dining habits`]: "Adventurous Diners",
         [`entertainment preference`]: "Outdoor Enthusiasts",
@@ -119,7 +119,7 @@ export const siteData = [
         ["social media usage"]: "Heavy Social Media Users",
         ["podcast listenership"]: "Regular Podcast Listeners",
       },
-      [`ooh behavior`]: {
+      [`pasyal-ing behavior`]: {
         [`shopping behavior`]: "Efficient Shoppers",
         [`dining habits`]: "Family-Oriented Diners",
         [`entertainment preference`]: "Sports Enthusiasts",
@@ -152,7 +152,7 @@ export const siteData = [
         ["social media usage"]: "Heavy Social Media Users",
         ["podcast listenership"]: "Regular Podcast Listeners",
       },
-      [`ooh behavior`]: {
+      [`pasyal-ing behavior`]: {
         [`shopping behavior`]: "Efficient Shoppers",
         [`dining habits`]: "Family-Oriented Diners",
         [`entertainment preference`]: "Sports Enthusiasts",
@@ -185,7 +185,7 @@ export const siteData = [
         ["social media usage"]: "Occasional Social Media Users",
         ["podcast listenership"]: "Occasional Podcast Listeners",
       },
-      [`ooh behavior`]: {
+      [`pasyal-ing behavior`]: {
         [`shopping behavior`]: "Selective Shoppers",
         [`dining habits`]: "Quality-Conscious Diners",
         [`entertainment preference`]: "Cultural Events Attendees",
@@ -218,7 +218,7 @@ export const siteData = [
         ["social media usage"]: "Heavy Social Media Users",
         ["podcast listenership"]: "Occasional Podcast Listeners",
       },
-      [`ooh behavior`]: {
+      [`pasyal-ing behavior`]: {
         [`shopping behavior`]: "Selective Shoppers",
         [`dining habits`]: "Quality-Conscious Diners",
         [`entertainment preference`]: "Cultural Events Attendees",
@@ -251,7 +251,7 @@ export const siteData = [
         ["social media usage"]: "Heavy Social Media Users",
         ["podcast listenership"]: "Regular Podcast Listeners",
       },
-      [`ooh behavior`]: {
+      [`pasyal-ing behavior`]: {
         [`shopping behavior`]: "Impulse Buyers",
         [`dining habits`]: "Casual Diners",
         [`entertainment preference`]: "Tech and Gaming Enthusiasts",
@@ -284,7 +284,7 @@ export const siteData = [
         ["social media usage"]: "Occasional Social Media Users",
         ["podcast listenership"]: "Occasional Podcast Listeners",
       },
-      [`ooh behavior`]: {
+      [`pasyal-ing behavior`]: {
         [`shopping behavior`]: "Selective Shoppers",
         [`dining habits`]: "Quality-Conscious Diners",
         [`entertainment preference`]: "Cultural Events Attendees",
@@ -317,7 +317,7 @@ export const siteData = [
         ["social media usage"]: "Heavy Social Media Users",
         ["podcast listenership"]: "Regular Podcast Listeners",
       },
-      [`ooh behavior`]: {
+      [`pasyal-ing behavior`]: {
         [`shopping behavior`]: "Selective Shoppers",
         [`dining habits`]: "Quality-Conscious Diners",
         [`entertainment preference`]: "Cultural Events Attendees",
@@ -350,7 +350,7 @@ export const siteData = [
         ["social media usage"]: "Heavy Social Media Users",
         ["podcast listenership"]: "Occasional Podcast Listeners",
       },
-      [`ooh behavior`]: {
+      [`pasyal-ing behavior`]: {
         [`shopping behavior`]: "Selective Shoppers",
         [`dining habits`]: "Quality-Conscious Diners",
         [`entertainment preference`]: "Cultural Events Attendees",
@@ -383,7 +383,7 @@ export const siteData = [
         ["social media usage"]: "Heavy Social Media Users",
         ["podcast listenership"]: "Regular Podcast Listeners",
       },
-      [`ooh behavior`]: {
+      [`pasyal-ing behavior`]: {
         [`shopping behavior`]: "Exploratory Shoppers",
         [`dining habits`]: "Adventurous Diners",
         [`entertainment preference`]: "Outdoor Enthusiasts",
@@ -552,79 +552,87 @@ export const demographics = [
     value: "Low Click-through Rates",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "shopping behavior",
     value: "Impulse Buyers",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "shopping behavior",
     value: "Intentional Shoppers",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "shopping behavior",
     value: "Exploratory Shoppers",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "shopping behavior",
     value: "Efficient Shoppers",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "shopping behavior",
     value: "Selective Shoppers",
   },
-  { category: "ooh behavior", key: "dining habits", value: "Frequent Diners" },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
+    key: "dining habits",
+    value: "Frequent Diners",
+  },
+  {
+    category: "pasyal-ing behavior",
     key: "dining habits",
     value: "Selectively Dine Out",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "dining habits",
     value: "Adventurous Diners",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "dining habits",
     value: "Family-Oriented Diners",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "dining habits",
     value: "Quality-Conscious Diners",
   },
-  { category: "ooh behavior", key: "dining habits", value: "Casual Diners" },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
+    key: "dining habits",
+    value: "Casual Diners",
+  },
+  {
+    category: "pasyal-ing behavior",
     key: "entertainment preference",
     value: "Event Attendees",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "entertainment preference",
     value: "Arts and Culture Enthusiasts",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "entertainment preference",
     value: "Outdoor Enthusiasts",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "entertainment preference",
     value: "Sports Enthusiasts",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "entertainment preference",
     value: "Cultural Events Attendees",
   },
   {
-    category: "ooh behavior",
+    category: "pasyal-ing behavior",
     key: "entertainment preference",
     value: "Tech and Gaming Enthusiasts",
   },
@@ -677,5 +685,160 @@ export const demographics = [
     category: "commuting behavior",
     key: "commuting frequency",
     value: "Seldom",
+  },
+];
+
+export const billboardData = [
+  {
+    location: "Central Square 3D LED BGC",
+    latitude: 14.551744,
+    longitude: 121.047957,
+    type: "Digital",
+  },
+  {
+    location: "C5 BAGONG ILOG NB (BORJA)",
+    latitude: 14.5647092,
+    longitude: 121.0699527,
+    type: "Digital",
+  },
+  {
+    location: "C5 BAGONG ILOG SB (Menzie)",
+    latitude: 14.5670153,
+    longitude: 121.0703566,
+    type: "Digital",
+  },
+  {
+    location: "Davao LED",
+    latitude: 7.0869827,
+    longitude: 125.6104221,
+    type: "Digital",
+  },
+  {
+    location: "EDSA GUADA LED",
+    latitude: 14.5705032,
+    longitude: 121.046417,
+    type: "Digital",
+  },
+  {
+    location: "Las Pinas LED (Commerce - Daang Hari)",
+    latitude: 14.423378,
+    longitude: 121.022342,
+    type: "Digital",
+  },
+  {
+    location: "One Bonifacio High Street LED",
+    latitude: 14.551399,
+    longitude: 121.047685,
+    type: "Digital",
+  },
+  {
+    location: "EDSA Belson",
+    latitude: 14.5705032,
+    longitude: 121.046417,
+    type: "Digital",
+  },
+  {
+    location: "ELS 1 (CAMP AGUINALDO GATE 4)",
+    latitude: 14.6035322,
+    longitude: 121.0587706,
+    type: "Classic",
+  },
+  {
+    location: "Paseo De Magallanes LED",
+    latitude: 14.538741,
+    longitude: 121.017272,
+    type: "Classic",
+  },
+  {
+    location: "COMMEX BLDG., EDSA COR. ARELLANO ST., CALOOCAN CITY/1",
+    latitude: 14.65739,
+    longitude: 120.9929,
+    type: "Classic",
+  },
+  {
+    location:
+      "LOL REALTY BLDG., EDSA COR ASUNCION ST., BRGY. 83, ZONE 8, MONUMENTO, CALOOCAN/1",
+    latitude: 14.65732,
+    longitude: 120.987,
+    type: "Classic",
+  },
+  {
+    location:
+      "VV SOLIVEN SHOPPING COMPLEX BLDG. I, EDSA, BRGY. NORTH EAST GREENHILLS, SAN JUAN/2",
+    latitude: 14.6059,
+    longitude: 121.0571,
+    type: "Classic",
+  },
+  {
+    location:
+      "VV SOLIVEN SHOPPING COMPLEX BLDG. I, EDSA, BRGY. NORTH EAST GREENHILLS, SAN JUAN/1",
+    latitude: 14.60495,
+    longitude: 121.0575,
+    type: "Classic",
+  },
+  {
+    location:
+      "GENESIS BUS STATION EDSA COR. NEW YORK ST. BRGY. E. RODRIGUEZ, QUEZON CITY/2",
+    latitude: 14.62496,
+    longitude: 121.0486,
+    type: "Classic",
+  },
+  {
+    location:
+      "GENESIS BUS STATION EDSA COR. NEW YORK ST. BRGY. E. RODRIGUEZ, QUEZON CITY/1",
+    latitude: 14.62608,
+    longitude: 121.0481,
+    type: "Classic",
+  },
+  {
+    location: "EVEREST LUMBER BLDG. #663 EDSA, CUBAO, QC/3",
+    latitude: 14.62328,
+    longitude: 121.0491,
+    type: "Classic",
+  },
+  {
+    location: "EVEREST LUMBER BLDG. #663 EDSA, CUBAO, QC/2",
+    latitude: 14.62191,
+    longitude: 121.0501,
+    type: "Classic",
+  },
+  {
+    location: "EVEREST LUMBER BLDG. #663 EDSA, CUBAO, QC/1",
+    latitude: 14.6232,
+    longitude: 121.0491,
+    type: "Classic",
+  },
+  {
+    location:
+      "EDSA CENTRUM BLDG #817 EDSA COR. KAMUNING RD., BRGY. KAMUNING, QC (Inner Structure)/2",
+    latitude: 14.62937,
+    longitude: 121.0464,
+    type: "Classic",
+  },
+  {
+    location:
+      "TRINITY LUTHERAN CHURCH #835 EDSA COR. TIMOG AVE., BRGY. SACRED HEART, QC (Road Side)/2",
+    latitude: 14.63163,
+    longitude: 121.0453,
+    type: "Classic",
+  },
+  {
+    location:
+      "PANAY BLDG. #160 PANAY AVE. COR. EDSA, BRGY. SOUTH TRIANGLE, QC (Road-Side)/2",
+    latitude: 14.64346,
+    longitude: 121.0377,
+    type: "Classic",
+  },
+];
+
+const billboard = [
+  {
+    id: 0,
+    site: "",
+    area: "",
+    region: "",
+    fits_no: 0,
+    fits_rate: 0.0,
+    avg_monthly_impressions: 0,
   },
 ];
