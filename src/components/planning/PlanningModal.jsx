2f14103b503e5120b5e75a775ac311101d2a5e83
vncntkyl <kyle.rinoza2009@gmail.com>
2024-01-06 18:54:44 +0800

    feat: add new modal component for Planning page

---
+++ src/components/planning/PlanningModal.jsx
@@ -0,0 +1,35 @@
+import { Modal } from "flowbite-react";
+import PropTypes from "prop-types";
+
+function PlanningModal({ trigger, toggle, header, body, footer }) {
+  return (
+    <Modal
+      show={trigger}
+      dismissible
+      position="center"
+      theme={{
+        content: { base: "relative h-auto w-full p-4 animate-fade" },
+        body: { base: "p-2" },
+        footer: {
+          base: "flex items-center justify-end space-x-2 rounded-b border-gray-200 p-4 dark:border-gray-600",
+        },
+      }}
+      onClose={() => toggle(false)}
+      size="lg"
+    >
+      <Modal.Header>{header}</Modal.Header>
+      <Modal.Body>{body}</Modal.Body>
+      {footer && <Modal.Footer>{footer}</Modal.Footer>}
+    </Modal>
+  );
+}
+
+PlanningModal.propTypes = {
+  trigger: PropTypes.bool,
+  toggle: PropTypes.func,
+  header: PropTypes.node,
+  body: PropTypes.node,
+  footer: PropTypes.node,
+};
+
+export default PlanningModal;
