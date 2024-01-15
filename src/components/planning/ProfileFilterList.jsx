90c760086e49319348e7ca8d96246013dc7aa867
vncntkyl <kyle.rinoza2009@gmail.com>
2024-01-15 17:13:03 +0800

    feat: added loader

--- src/components/planning/ProfileFilterList.jsx
+++ src/components/planning/ProfileFilterList.jsx
@@ -3,6 +3,8 @@ import PlanningList from "./PlanningList";
 import { useEffect, useState } from "react";
 import { useService } from "../../config/services";
 import Tabs from "../../fragments/Tabs";
+import Skeleton from "~fragments/Skeleton";
+import Loader from "~fragments/Loader";
 
 function ProfileFilterList({
   toggleProfile,
@@ -20,23 +22,27 @@ function ProfileFilterList({
     };
     setup();
   }, []);
-  return (
-    demographics && (
-      <Tabs
-        tabs={["all", ...new Set(demographics.map((item) => item.category))]}
-        content={
-          <PlanningList
-            category={activeTab}
-            toggleProfile={toggleProfile}
-            search={setSearchQuery}
-            searchBuyergraphics={searchBuyergraphics}
-            data={demographics}
-          />
-        }
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
-      />
-    )
+  return demographics ? (
+    <Tabs
+      tabs={["all", ...new Set(demographics.map((item) => item.category))]}
+      content={
+        <PlanningList
+          category={activeTab}
+          toggleProfile={toggleProfile}
+          search={setSearchQuery}
+          searchBuyergraphics={searchBuyergraphics}
+          data={demographics}
+        />
+      }
+      activeTab={activeTab}
+      setActiveTab={setActiveTab}
+    />
+  ) : (
+    <div className="p-2 flex flex-col gap-2">
+      {[2, 0.5, 4, 4, 4, 4, 4].map((val, idx) => {
+        return <Loader key={idx} height={`${val}rem`} />;
+      })}
+    </div>
   );
 }
 
