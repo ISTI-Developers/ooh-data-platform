f1011e19a94497fd911ff4a703e764271bef8fc1
vncntkyl <kyle.rinoza2009@gmail.com>
2024-01-17 07:53:07 +0800

    build: added scrollbar rounded compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,5 +50,5 @@ export default {
       },
     },
   },
-  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
+  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")({ nocompatible: true })],
 };
